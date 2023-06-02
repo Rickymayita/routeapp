29d816b565f3f69a8bab0e3cab4fff127271b0f6
Ricardo Ariel Maya <rickymayita@hotmail.com>
2023-06-02 16:33:38 -0300

    Go and activate the selected place

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -1,17 +1,30 @@
-import { useContext } from "react"
-import { PlacesContext } from "../context"
+import { useContext, useState } from "react"
+import { MapContext, PlacesContext } from "../context"
 import { LoadingPlaces } from "./";
+import { Feature } from "../interfaces/places";
 
 
 export const SearchResults = () => {
 
     const { places, isLoadingPlaces } = useContext(PlacesContext);
+    const { map } = useContext(MapContext)
 
-    if( isLoadingPlaces ) {
+    const [activeId, setActiveId] = useState('')
+
+    const onPlaceClicked = (place: Feature) => {
+        const [lng, lat] = place.center;
+        setActiveId(place.id);
+        map?.flyTo({
+            zoom: 14,
+            center: [lng, lat]
+        })
+    }
+
+    if (isLoadingPlaces) {
         return <LoadingPlaces />;
     }
 
-    if ( places.length === 0 ) {
+    if (places.length === 0) {
         return <></>;
     }
 
@@ -21,20 +34,20 @@ export const SearchResults = () => {
             {
                 places.map(place => (
                     <li
-                        key={ place.id }
-                        className="list-group-item list-group-item-action"
+                        key={place.id}
+                        className={`list-group-item list-group-item-action pointer ${(activeId === place.id) ? 'active' : ''}`}
+                        onClick={() => onPlaceClicked(place)}
                     >
-                        <h6>{ place.text_en }</h6>
+                        <h6>{place.text_en}</h6>
                         <p
-                            className="text-muted"
                             style={{
                                 fontSize: '12px'
                             }}
                         >
                             {place.place_name}
                         </p>
-                        <button className="btn btn-outline-primary btn-sm">
-                            Addresses
+                        <button className={`btn btn-sm ${ activeId === place.id ? 'btn-outline-light':'btn-outline-primary' } `}>
+                            Route
                         </button>
 
                     </li>
