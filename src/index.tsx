a2439ba5e19e6f03e9a9e7dd036986951dec56f6
Ricardo Ariel Maya <rickymayita@hotmail.com>
2023-06-05 19:00:30 -0300

    add ignore

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,12 @@
+/* eslint import/no-webpack-loader-syntax: off */
+
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouteApp } from './RouteApp';
 
-
-import mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+//@ts-ignore
+import mapboxgl from '!mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
  
 mapboxgl.accessToken = 'pk.eyJ1IjoibXJpY2t5dGVjaCIsImEiOiJjbGlibnhrZWIwOTZ5M2RuMjdsZG1id2doIn0.V_EpM_CUx9PRJ68bOgdzzw';
 
